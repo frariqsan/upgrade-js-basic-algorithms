441abe0bb330353f50bc34857a2e55f364a9364f
frariqsan <frariqsan@gmail.com>
2022-05-21 18:17:05 +0200

    Iteration finished

--- arrays/array1-6.js
+++ arrays/array1-6.js
@@ -1,2 +1,4 @@
 //Elimina el segundo elemento del array y muestra el array por consola.
 const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+//Se hace mediante un for of 
